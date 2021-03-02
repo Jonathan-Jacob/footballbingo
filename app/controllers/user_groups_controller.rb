dcc81a1c4d25cd8bcc18793e445db79be3b8cd1d
Jonathan Jacob <jjjscf@gmail.com>
2021-03-02 17:35:59 +0100

    changes with valerie

---
+++ app/controllers/user_groups_controller.rb
@@ -0,0 +1,23 @@
+class UserGroupsController < ApplicationController
+  before_action :set_group, only: :create
+
+  def create
+    @user_group = UserGroup.new(user_group_params)
+    authorize @user_group
+    if @user_group.save
+      redirect_to_group_path(@group)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_group_params
+    params.require(:user_group).permit(:user_id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+end
--- db/migrate/20210302103759_create_user_groups.rb
+++ db/migrate/20210302103759_create_user_groups.rb
@@ -1,6 +1,6 @@
-class CreateUsersGroups < ActiveRecord::Migration[6.0]
+class CreateUserGroups < ActiveRecord::Migration[6.0]
   def change
-    create_table :users_groups do |t|
+    create_table :user_groups do |t|
       t.references :user, null: false, foreign_key: true
       t.references :group, null: false, foreign_key: true
 
