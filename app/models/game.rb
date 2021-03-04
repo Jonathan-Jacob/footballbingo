5521ec550dd5f66b994e7046725235a8d1024a27
Stefan Koch <webokoch@gmx.de>
2021-03-04 10:45:46 +0100

    match model validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ApplicationRecord
   belongs_to :match
   belongs_to :group
   has_many :bingo_cards
-  validates :match_id, uniqueness: true
+  validates :match, uniqueness: { scope: :group, message: 'is already in group' }
 end
