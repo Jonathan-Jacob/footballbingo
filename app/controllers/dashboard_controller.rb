a7be9cdf9ad178950a76627a47137e3e5f0a017d
Stefan Koch <webokoch@gmx.de>
2021-03-08 09:35:08 +0100

    updated display of games and groups in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
   # skip_before_action :authenticate_user!
   def show
-    @groups = Group.all
-    @games = @groups.map do |group|
-      group.games
+    @user_groups = UserGroup.where(user: current_user)
+    @games = @user_groups.map do |user_group|
+      user_group.group.games
     end.flatten
   end
 end
