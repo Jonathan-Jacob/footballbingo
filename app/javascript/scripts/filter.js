7d84b9fd0acddf8d18205f0655abd38792457a8e
Jonathan Jacob <jjjscf@gmail.com>
2021-03-11 15:08:28 +0100

    presentable

--- app/javascript/scripts/filter.js
+++ app/javascript/scripts/filter.js
@@ -1,20 +1,27 @@
 import Rails from '@rails/ujs';
 
 const updateMatches = (event) => {
-  const matchesInput = document.querySelector("#game_match_id")
-  Rails.ajax({
-    type: "GET",
-    url: "/competition_matches",
-    data: `[competition_id]=${event.currentTarget.value}`,
-    success: (data) => {
-      let optionsHtml = '<option value=""></option>'
-      data.matches.forEach((match) => {
-        optionsHtml += `<option value="${match.id}">${match.team_1} vs ${match.team_2} at ${match.date_time}</option>`
-      });
-      matchesInput.innerHTML = optionsHtml
-    }
-  })
-}
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+   const matchesInput = document.querySelector("#game_match_id")
+   Rails.ajax({
+     type: "GET",
+     url: "/competition_matches",
+     data: `[competition_id]=${event.currentTarget.value}`,
+     success: (data) => {
+       let optionsHtml = '<option value=""></option>'
+       data.matches.forEach((match) => {
+         const match_date = new Date(match.date_time)
+         console.log(match_date)
+         const day = match_date.getDate()
+         const monthIndex = match_date.getMonth()
+         const hours = match_date.getHours()
+         const years = 1900 + match_date.getYear()
+         optionsHtml += `<option value="${match.id}">${match.team_1} vs ${match.team_2} - ${day}.${months[monthIndex]} ${years} at ${hours}H</option>`
+       });
+       matchesInput.innerHTML = optionsHtml
+     }
+   })
+ }
 
 
 const initFilter = () => {
