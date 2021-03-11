749ba06af4732d859a938c0dbed8d250d373b3ba
Stefan Koch <webokoch@gmx.de>
2021-03-11 18:30:33 +0100

    js changes

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -2,14 +2,16 @@ import consumer from "./consumer";
 
 const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
-  if (messagesContainer) {
+  const inputField = document.getElementById('message_content');
+
+  if (messagesContainer && inputField) {
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
     const id = messagesContainer.dataset.chatroomId;
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
         messagesContainer.insertAdjacentHTML('beforeend', data);
         messagesContainer.scrollTop = messagesContainer.scrollHeight;
-        const form = document.getElementById('message_content').value = "";
+        inputField.value = "";
       },
     });
   }
