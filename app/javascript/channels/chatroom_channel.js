651978469362f2e253f1a0a697effabfcada5840
Stefan Koch <webokoch@gmx.de>
2021-03-11 17:41:56 +0100

    added JS to scroll down when receiving new message

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,24 +1,5 @@
 import consumer from "./consumer";
 
-const pickMessageSide = () => {
- // let messages = document.querySelectorAll(".message-container");
-  let messages = document.querySelectorAll(".message-container");
-  let container = document.getElementById("messages");
-
-  if (messages.length > 0 && container) {
-      // messages.forEach( (message) => {
-        let message = messages[messages.length - 1];
-        if (message.dataset.senderId === container.dataset.currentUser ) {
-          message.classList.remove('buddy-message');
-          message.classList.add('user-message');
-        } else {
-          message.classList.remove('user-message');
-          message.classList.add('buddy-message');
-        }
-      // })
-  }
-}
-
 const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
@@ -26,9 +7,8 @@ const initChatroomCable = () => {
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
-        console.log(data);
         messagesContainer.insertAdjacentHTML('beforeend', data);
-        // pickMessageSide();
+        messagesContainer.scrollTop = messagesContainer.scrollHeight;
       },
     });
   }
